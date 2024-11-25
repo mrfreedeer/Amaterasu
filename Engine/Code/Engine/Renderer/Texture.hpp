1dfaa87a5433823aa72381148fe8f1a0888c2ba2
Juan Ospina Bustamante <juano.bustam@gmail.com>
2024-11-24 16:00:47 -0800

    [INTERFACES] - Reworked texture class to work with new work flow

--- Engine/Code/Engine/Renderer/Texture.hpp
+++ Engine/Code/Engine/Renderer/Texture.hpp
@@ -1,63 +1,60 @@
 #pragma once
 #include "Engine/Math/IntVec2.hpp"
-#include "Engine/Renderer/ResourceView.hpp"
 #include "Engine/Core/Rgba8.hpp"
 #include "Engine/Renderer/GraphicsCommon.hpp"
 #include <vector>
 #include <string>
 
 class Resource;
-class TextureView;
+struct ResourceView;
 class Renderer;
 
+enum ResourceBindFlagBit : unsigned int {
+	RESOURCE_BIND_NONE = 0,
+	RESOURCE_BIND_SHADER_RESOURCE_BIT = (1 << 0),
+	RESOURCE_BIND_RENDER_TARGET_BIT = (1 << 1),
+	RESOURCE_BIND_DEPTH_STENCIL_BIT = (1 << 2),
+	RESOURCE_BIND_UNORDERED_ACCESS_VIEW_BIT = (1 << 3),
+	RESOURCE_BIND_CONSTANT_BUFFER_VIEW_BIT = (1 << 4),
+	RESOURCE_BIND_ALL_BUFFER_VIEWS = (0b1001), // SRV AND UAV
+	RESOURCE_BIND_ALL_TEXTURE_VIEWS = (0b1011), // SRV, RTV AND UAV
+};
 
-
+typedef unsigned int ResourceBindFlag;
 
 struct TextureCreateInfo {
 	std::string m_name = "Unnamed Texture";
 	Renderer* m_owner = nullptr;
-	IntVec2 m_dimensions = IntVec2::ZERO;
-	size_t m_stride = 0;
-	ResourceBindFlag m_bindFlags = RESOURCE_BIND_NONE;
-	TextureFormat m_format = TextureFormat::R8G8B8A8_UNORM;
-	TextureFormat m_clearFormat = TextureFormat::R8G8B8A8_UNORM;
-	bool m_isMultiSample = false;
 	Resource* m_handle = nullptr;
 	void* m_initialData = nullptr;
+	ResourceBindFlag m_bindFlags = RESOURCE_BIND_NONE;
+	bool m_isMultiSample = false;
+	size_t m_stride = 0;
+	IntVec2 m_dimensions = IntVec2::ZERO;
+	TextureFormat m_format = TextureFormat::R8G8B8A8_UNORM;
+	TextureFormat m_clearFormat = TextureFormat::R8G8B8A8_UNORM;
 	Rgba8 m_clearColour = Rgba8();
 
 };
 
 
 class Texture {
-friend class Renderer;
+	friend class Renderer;
 public:
-	/// <summary>
-	/// Checks if view has been created, or creates it otherwise
-	/// </summary>
-	ResourceView* GetOrCreateView(ResourceBindFlagBit viewType);
-	bool IsBindCompatible(ResourceBindFlag bindFlag) const;
-	IntVec2 GetDimensions() const;
-	Resource* GetResource() const;
-	ID3D12Resource* GetRawResource() const;
-	Rgba8 const GetClearColour() const;
-	std::string const& GetImageFilePath() const { return m_name; }
-	void TransitionTo(D3D12_RESOURCE_STATES newState, ID3D12GraphicsCommandList* commList);
-	void TransitionTo(D3D12_RESOURCE_STATES newState, ComPtr<ID3D12GraphicsCommandList> commList);
+	ResourceView* GetRenderTargetView() const;
+	ResourceView* GetDepthStencilView() const;
+	ResourceView* GetShaderResourceView() const;
+	TextureFormat GetFormat() const { return m_info.m_format; }
+	bool IsRenderTargetCompatible() const { return m_info.m_bindFlags & RESOURCE_BIND_RENDER_TARGET_BIT; }
+	bool IsShaderResourceCompatible() const { return m_info.m_bindFlags & RESOURCE_BIND_SHADER_RESOURCE_BIT; }
+	bool IsDepthStencilCompatible() const { return m_info.m_bindFlags & RESOURCE_BIND_DEPTH_STENCIL_BIT; }
+	bool IsUnorderedAccessCompatible() const { return m_info.m_bindFlags & RESOURCE_BIND_UNORDERED_ACCESS_VIEW_BIT; }
 private:
-	/// Views are created in DX12 but they're not a separate structure
-	ResourceView* CreateShaderResourceView();
-	ResourceView* CreateRenderTargetView();
-	ResourceView* CreateDepthStencilView();
-	ResourceView* CreateUnorderedAccessView();
 	Texture();
 	Texture(TextureCreateInfo const& createInfo);
 	~Texture();
 private:
-	Renderer* m_owner = nullptr;
-	std::string m_name = "Unammed Texture";
-	TextureCreateInfo m_creationInfo;
-	Resource* m_handle = nullptr;
-	Resource* m_uploadRsc = nullptr;
-	std::vector<ResourceView*> m_views;
+	Resource* m_rsc = nullptr;
+	TextureCreateInfo m_info = {};
+
 };
