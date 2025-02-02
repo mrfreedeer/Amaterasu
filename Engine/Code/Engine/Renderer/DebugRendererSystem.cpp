d22ccde1826c1107ccc26472eb55c05ac50b1d96
Juan Ospina Bustamante <juano.bustam@gmail.com>
2025-02-01 17:29:25 -0800

    [ENGINE - RENDERER] - Added missing important files for Renderer classes used for sprite animations, fonts, and vertex representation

---
+++ Engine/Code/Engine/Renderer/DebugRendererSystem.cpp
@@ -0,0 +1,123 @@
+#include "Engine/Renderer/DebugRendererSystem.hpp"
+#include "Engine/Renderer/Renderer.hpp"
+#include "Engine/Core/VertexUtils.hpp"
+#include "Engine/Renderer/DebugShape.hpp"
+#include "Engine/Core/Clock.hpp"
+#include "Engine/Renderer/Billboard.hpp"
+#include "Engine/Renderer/BitmapFont.hpp"
+#include <vector>
+
+
+void DebugRenderSystemStartup(const DebugRenderConfig& config)
+{
+	
+}
+
+void DebugRenderSystemShutdown()
+{
+
+}
+
+void DebugRenderSetVisible()
+{
+
+}
+
+void DebugRenderSetHidden()
+{
+
+}
+
+void DebugRenderClear()
+{
+	
+}
+
+void DebugRenderSetParentClock(Clock& parent)
+{
+	
+}
+
+void DebugRenderSetTimeDilation(double timeDilation)
+{
+}
+
+Clock const& DebugRenderGetClock()
+{
+	
+}
+
+void DebugRenderBeginFrame()
+{
+}
+
+void DebugRenderWorld(const Camera& camera)
+{
+}
+
+void DebugRenderScreen(const Camera& camera)
+{
+
+}
+
+void DebugRenderEndFrame()
+{
+	
+}
+
+void DebugAddWorldPoint(const Vec3& pos, float radius, float duration, const Rgba8& startColor, const Rgba8& endColor, DebugRenderMode mode, int stacks, int slices)
+{
+
+}
+
+void DebugAddWorldLine(const Vec3& start, const Vec3& end, float radius, float duration, const Rgba8& startColor, const Rgba8& endColor, DebugRenderMode mode)
+{
+
+}
+
+void DebugAddWorldWireCylinder(const Vec3& base, const Vec3& top, float radius, float duration, const Rgba8& startColor, const Rgba8& endColor, DebugRenderMode mode)
+{
+
+}
+
+void DebugAddWorldWireSphere(const Vec3& center, float radius, float duration, const Rgba8& startColor, const Rgba8& endColor, DebugRenderMode mode, int stacks, int slices)
+{
+	
+}
+
+void DebugAddWorldArrow(const Vec3& start, const Vec3& end, float radius, float duration, const Rgba8& baseColor, const Rgba8& startColor, const Rgba8& endColor, DebugRenderMode mode)
+{
+	
+}
+
+void DebugAddWorldBox(const AABB3& bounds, float duration, const Rgba8& startColor, const Rgba8& endColor, DebugRenderMode mode)
+{
+	
+}
+
+void DebugAddWorldBasis(const Mat44& basis, float duration, const Rgba8& startColor, const Rgba8& endColor, DebugRenderMode mode)
+{
+
+}
+
+void DebugAddWorldText(const std::string& text, const Mat44& transform, float textHeight, const Vec2& alignment, float duration, const Rgba8& startColor, const Rgba8& endColor, DebugRenderMode mode)
+{
+	
+}
+
+void DebugAddWorldBillboardText(const std::string& text, const Vec3& origin, float textHeight, const Vec2& alignment, float duration, const Rgba8& startColor, const Rgba8& endColor, DebugRenderMode mode)
+{
+
+}
+
+void DebugAddScreenText(const std::string& text, const Vec2& position, float duration, const Vec2& alignment, float size, const Rgba8& startColor, const Rgba8& endColor)
+{
+	
+}
+
+void DebugAddMessage(const std::string& text, float duration, const Rgba8& startColor, const Rgba8& endColor)
+{
+
+}
+
+
