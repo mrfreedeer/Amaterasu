2db5185ad16af94ea2ac84b5743cfed3e6ada938
Juan Ospina Bustamante <juano.bustam@gmail.com>
2025-04-20 22:20:18 -0700

    [ENGINE] - Refactored fence to have buffering internally for proper frame sync

--- Engine/Code/Engine/Renderer/Interfaces/Fence.cpp
+++ Engine/Code/Engine/Renderer/Interfaces/Fence.cpp
@@ -3,32 +3,42 @@
 #include "Engine/Renderer/GraphicsCommon.hpp"
 #include <d3d12.h>
 
-Fence::Fence(CommandQueue* commandQueue, unsigned int initialValue /*= 0*/):
-	m_fenceValue(initialValue),
-	m_commandQueue(commandQueue)
+Fence::Fence(CommandQueue* commandQueue, unsigned int initialValue /*= 0*/, unsigned int bufferCount /*= 1*/) :
+	m_commandQueue(commandQueue),
+	m_bufferCount(bufferCount)
 {
-	
+	m_fenceValues = new unsigned int[m_bufferCount];
+	memset(m_fenceValues, 0, sizeof(unsigned int) * m_bufferCount);
+	m_fenceValues[0] = initialValue;
+
 
 }
 
 Fence::~Fence()
 {
+	delete[] m_fenceValues;
 	DX_SAFE_RELEASE(m_fence);
 }
 
 unsigned int Fence::Signal()
 {
-	m_fenceValue++;
-	m_commandQueue->Signal(this, m_fenceValue);
-	return m_fenceValue;
+	unsigned int currentValue = m_fenceValues[m_currentIndex];
+	m_commandQueue->Signal(this, currentValue);
+	return currentValue;
 }
 
-void Fence::Wait(unsigned int waitValue)
+void Fence::Wait()
 {
-	if (m_fence->GetCompletedValue() < waitValue)
+	unsigned int currentValue = m_fenceValues[m_currentIndex];
+	m_currentIndex = (m_currentIndex + 1) % m_bufferCount;
+
+	if (m_fence->GetCompletedValue() < m_fenceValues[m_currentIndex])
 	{
-		ThrowIfFailed(m_fence->SetEventOnCompletion(m_fenceValue, m_fenceEvent), "FAILED TO SET EVENT ON COMPLETION");
+		ThrowIfFailed(m_fence->SetEventOnCompletion(m_fenceValues[m_currentIndex], m_fenceEvent), "FAILED TO SET EVENT ON COMPLETION");
 		WaitForSingleObject(m_fenceEvent, INFINITE);
 	}
+
+	m_fenceValues[m_currentIndex] = currentValue + 1;
 }
+
 
--- Engine/Code/Engine/Renderer/Interfaces/Fence.hpp
+++ Engine/Code/Engine/Renderer/Interfaces/Fence.hpp
@@ -7,16 +7,26 @@ class Fence {
 friend class Renderer;
 friend class CommandQueue;
 public:
-	Fence(CommandQueue* fenceManager, unsigned int initialValue = 0);
+
+	/// <summary>
+	/// Fence to wait for work to be done, can be used in buffering as well
+	/// </summary>
+	/// <param name="fenceManager"></param>
+	/// <param name="bufferCount"></param>
+	/// <param name="initialValue"></param>
+	Fence(CommandQueue* fenceManager, unsigned int initialValue = 1, unsigned int bufferCount = 2);
 	~Fence();
 
 	unsigned int Signal();
-	void Wait(unsigned int waitValue);
-	unsigned int GetFenceValue() const { return m_fenceValue; }
+	void Wait();
+	unsigned int GetFenceValue() const { return m_fenceValues[m_currentIndex]; }
+
 private:
 	ID3D12Fence1* m_fence = nullptr;
 	CommandQueue* m_commandQueue = nullptr;
 	void* m_fenceEvent = nullptr;
-	unsigned int m_fenceValue = 0;
+	unsigned int* m_fenceValues = nullptr;
+	unsigned int m_currentIndex = 0;
+	unsigned int m_bufferCount = 0;
 
 };
