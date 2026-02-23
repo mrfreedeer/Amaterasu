f572ef0aa0c8a860c0714a3d22e20c8274ed5621
Juan Ospina Bustamante <juano.bustam@gmail.com>
2026-02-22 16:58:18 -0800

    [ENGINE] - Fixed seemingly slight memory leak with resource views going unchecked for deletion

--- Engine/Code/Engine/Renderer/Interfaces/Resource.cpp
+++ Engine/Code/Engine/Renderer/Interfaces/Resource.cpp
@@ -26,4 +26,9 @@ TransitionBarrier Resource::GetTransitionBarrier(ResourceStates afterState)
 Resource::~Resource()
 {
 	DX_SAFE_RELEASE(m_rawRsc);
+	if (m_dsv) { delete m_dsv; m_dsv = nullptr; }
+	if (m_uav) { delete m_uav; m_uav = nullptr; }
+	if (m_srv) { delete m_srv; m_srv = nullptr; }
+	if (m_rtv) { delete m_rtv; m_rtv = nullptr; }
+	if (m_cbv) { delete m_cbv; m_cbv = nullptr; }
 }
